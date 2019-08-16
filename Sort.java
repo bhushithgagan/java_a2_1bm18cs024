79b6c58176a4685d28405eb0db249ac9f721588f
bhushithgagan <42802614+bhushithgagan@users.noreply.github.com>
2019-08-16 20:08:34 +0530

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -6,12 +6,22 @@ System.out.println("Enter the size of array");
 Scanner sc = new Scanner (System.in);
 int n = sc.nextInt();
 system.out.println("Enter the array elements");
-int a[] = new int[n];
-for (int i=0; i<n;i++)
-{
-a[i] = sc.nextInt();
-}
-for (int i=0;i<n;i++)
-{
-if 
+for(i=0;i<n;i++)
+a[i]=sc.nextInt();
+for(i=0;i<n-1;i++)
+	{
+	for(j=i+1;j<n;j++)
+	{
+	if(a[i]>a[j])
+	{
+		temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+	}
+		}		
+		}
+	System.out.println("The sorted array is:");
+	for(i=0;i<n;i++)
+	System.out.print(a[i]+" ");
+		}
 
