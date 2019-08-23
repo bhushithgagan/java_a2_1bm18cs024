d474c407d2281e35a018a3d879e08a963554b633
bhushithgagan <42802614+bhushithgagan@users.noreply.github.com>
2019-08-23 15:38:04 +0530

    Add files via upload

--- Stud.java
+++ Stud.java
@@ -1,65 +1,77 @@
 import java.util.*;
-class Stud{
-
-int m[] = new int[n];
-int gp[] = new int[n];
-int cred[] = new int[n];
-String name,usn;
-int n,i;
-
-void Input(){
-Scanner sc = new Scanner(System.in);
-System.outln("Enter the Student name ");
-name = sc.next();
-System.outln("Enter the number of subjects");
-n = sc.nextInt();
-System.outln("Enter the Student USN");
-usn = sc.next();
-for(int i = 0;i<n;i++)
+class Student
 {
-System.outln("Enter the Student Marks");
-m[i] = sc.nextInt();
-System.outln("Enter the credits");
-cred[i] = sc.nextInt();
+	String usn, name;
+	int credits[], marks[], n;
+
+	void Input()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of subjects");
+		n=sc.nextInt();
+		credits = new int[n];
+		marks = new int[n];
+		System.out.println("Enter your name and usn");
+		name = sc.next();
+		usn = sc.next();
+		System.out.println("Enter the credits and the marks respectively");
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Subject "+ (i+1)+ ":");
+			credits[i]=sc.nextInt();
+			marks[i]=sc.nextInt();
+		}
+	}
+	
+	void Display(float sgpa)
+	{
+		System.out.println("Name -> " + name);
+		System.out.println("USN -> " + usn);
+		System.out.println("SGPA -> " + sgpa);
+	}
+	float Calculate_sgpa()
+	{
+		int credit_sum=0;
+		int grade_points[]= new int[n];
+		int total=0;
+		float sgpa;
+		for(int i=0;i<n;i++)
+		{
+			credit_sum+=credits[i];
+			
+			if(marks[i]>=0 && marks[i]<=40)
+				grade_points[i]=0;
+			else if(marks[i]>40 && marks[i]<=60)
+				grade_points[i]=7;
+			else if(marks[i]>60 && marks[i]<=75)
+				grade_points[i]=8;
+			else if(marks[i]>75 && marks[i]<=90)
+				grade_points[i]=9;
+			else
+				grade_points[i]=10;
+
+
+		//Calculate total
+		total= total + (grade_points[i] * credits[i]);
+		}
+
+		//Calculate sgpa
+		sgpa = (float)total/credit_sum;
+
+		return sgpa;
+	}
 }
 
-}
-
-void Cal(){
-for (i = 0;i<n;i++)
+class Stud
 {
-if (m[i]>=90 && m[i]<=100)
-gp=10;
-else if (m[i]>=75 && m[i]<90)
-gp=9;
-else if (m[i]>=60 && m[i]<75)
-gp = 8;
-else if (m[i]>=50 && m[i]<60)
-gp = 7;
-else if (m[i]>=40 && m[i]<50)
-gp = 6;
-else gp = 0;
+	public static void main(String args[])
+	{
+
+		Student obj = new Student();
+		obj.Input();
+		float sgpa = obj.Calculate_sgpa();
+		obj.Display(sgpa);
+	}
 }
-
-sgpa = gp*cred[i]/
-}
-
-void Output(){
-System.outln("Student name " + name);
-System.outln("Student usn" + usn);
-System.outln("SGPA" + sgpa);
-}
-
-class StudDemo(){
-public static void main(){
-
-Stud ob = new Stud();
-
-ob.Input();
-ob.Cal();
-ob.Output();
-}
-}
-
-
-
+		
+		
