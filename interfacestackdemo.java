223fb46ebf742a8205c627076fe9f65cfbcd367a
bhushithgagan <42802614+bhushithgagan@users.noreply.github.com>
2019-09-27 15:30:13 +0530

    Add files via upload

---
+++ interfacestackdemo.java
@@ -0,0 +1,44 @@
+import java.util.*;
+interface  Stack {
+	void push();
+	void pop();
+	}
+	
+class Stk implements Stack {
+	Scanner sc = new Scanner(System.in);
+	int t=-1,ele,n;
+	System.out.println("Enter the size of Stack: ");
+	n = sc.nextInt();
+	int[] s = new int[n];
+	public void push() {	
+	for(int i = 0; i<n; i++) {
+		System.out.println("Enter the " + (i+1) + "Element: " );
+	    ele = sc.nextInt();
+	    s[t+1] = ele;	
+	    }
+	   }
+	public void pop() {
+	if (t == n-1)
+	{
+	System.out.println("Stack Empty!\n);
+	return;
+	}
+	ele = s[t];
+	s[t--];
+	}
+   public void display() {
+   for(int i = t;i>0;i--) {
+   System.out.println(s[i]"\n");
+   }
+  }
+  
+class Stacktest implements Stack extends Stk {
+	if (t==n-1)
+	n = n * 2;
+	int[] t = new int[n];
+	public void push() {
+ 	for (int i = 0;i<n;i++) {
+ 	t[i] = s[i];
+ 	s = t ;
+ 	}
+ 		 
