cc1be1cd30bde76f82410351fc6adb722f80ca07
bhushithgagan <42802614+bhushithgagan@users.noreply.github.com>
2019-11-28 21:08:30 +0530

    Add files via upload

--- StudentexDemo.java
+++ StudentexDemo.java
@@ -2,7 +2,7 @@ import java.util.*;
 	class StudentexDemo {
 	public static void main(String args[]) {
 	String usn,name;
-	int marks[] = new int[3];
+	int marks[] = new int[5];
 	try {
 		if(args.length == 0 || args.length <0)
 		throw new NoArgsException("Zero Arguments");
@@ -11,16 +11,18 @@ import java.util.*;
 	System.out.println(e);
 		System.exit(0);
 	}
-	try {
-		for(int i = 0; i<3; i++) {
-		marks[i] = Integer.parseInt(args[i]);
+	try {	
+		name = args[0];
+		usn = args[1];
+		for(int i = 0;i<3;i++) {
+		marks[i] = Integer.parseInt(args[i+2]);
 		if(marks[i]<0)
 		throw new NoArgsException("INVALID ENTRY TRY AGAIN! : (Negative marks)");
 		}
-	System.out.println("Enter USN and name:\n");
+	/*System.out.println("Enter USN and name:\n");
 	Scanner sc = new Scanner(System.in);
 	usn = sc.next();
-	name  = sc.next();
+	name  = sc.next(); */
 	System.out.println("*** ENTERED DETAILS ***\n");
 	System.out.println("Name: " + name + "\nUSN: " + usn);
 	for(int i = 0;i<3;i++) {
